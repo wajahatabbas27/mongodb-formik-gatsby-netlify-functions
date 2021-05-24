c23bf02b5f6d26dfaaad24aaad02378b573b0d7f
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2021-05-24 17:39:07 +0500

    created post request and used formik in index.js and save data to mongodb

---
+++ functions/add/add.js
@@ -0,0 +1,57 @@
+var mongoose = require('mongoose');
+let connection = null;
+
+databaseConnection = async () => {
+  console.log("connection exists = ", connection !== null);
+  if (!connection) {
+    try {
+      mongoose.connect("mongodb+srv://WajahatAZ:Abihawajahat1!@jamstackcrudmongodb.2qgx1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+        useNewUrlParser: true, useUnifiedTopology: true,
+        bufferCommands: false, bufferMaxEntries: 0
+      });
+      connection = mongoose.connection;
+      await connection;
+      console.log('mongoose open for business');
+
+      //Define a schema
+      const studentSchema = new mongoose.Schema({
+        name: String,
+        age: Number
+      });
+
+      mongoose.models.Student || mongoose.model('Student', studentSchema);
+    }
+    catch (error) {
+      console.log("Error in connecting to database = ", error);
+    }
+  }
+  return connection;
+}
+
+exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  try {
+    // Only allow POST
+    if (event.httpMethod !== "POST") {
+      return { statusCode: 405, body: "Method Not Allowed" };
+    }
+    let reqObj = JSON.parse(event.body);
+
+    await databaseConnection();
+    const Student = mongoose.models.Student
+
+    const student1 = new Student({
+      name: reqObj.name,
+      age: reqObj.age
+    });
+    const result = await student1.save();
+    console.log("Entry Created and Inserted: " + result._id);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ id: `${result._id}` }),
+    }
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() }
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,56 @@
-import React from "react"
+import React, { useState } from "react"
+import { Formik, Form, ErrorMessage, Field } from 'formik';
+import TextField from '@material-ui/core/TextField';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [mydata, setData] = useState("");
+
+
+  return <div>
+    <div>Directory Addition Form</div>
+    <Formik initialValues={{
+      name: "",
+      age: 0
+    }}
+      onSubmit={(values) => {
+        console.log(values);
+        fetch(`/.netlify/functions/add`, {
+          method: 'post',
+          body: JSON.stringify(values)
+        })
+          .then(response => response.json())
+          .then(data => {
+            setData(data);
+            console.log("Data: " + JSON.stringify(data));
+
+          });
+      }}  >
+      {
+        (formik) => (
+
+          <Form onSubmit={formik.handleSubmit}>
+            <div>
+              <Field type="text" as={TextField} variant="outlined" label="Name::" name="name" id="name" />
+              <ErrorMessage name="name" render={(msg) => (
+                <span style={{ color: "red" }}>{msg}</span>
+              )} />
+            </div>
+            <div>
+              <Field type="number" as={TextField} label="Age:: " variant="filled" name="age" id="age" />
+              <ErrorMessage name="age" />
+            </div>
+            <div>
+              <button type="submit">Add</button>
+            </div>
+          </Form>
+
+
+        )
+      }
+
+
+    </Formik>
+
+    <div>{mydata.id}</div>
+  </div>
 }
